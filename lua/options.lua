416206a587046aa355583be268f186bf55bd62a2
Arvin Dev <dev.arvin123@gmail.com>
2025-03-19 17:46:01 +1000

    added relativeline numbers and refactored

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -20,7 +20,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 -- vim.schedule(function()
-  -- vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
 -- end)
 
 -- Enable break indent
@@ -68,15 +68,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-
 -- Toggle spell checking -> good for .md, who spell checks code
 vim.keymap.set('n', '<leader>ts', function()
   vim.opt.spell = not vim.o.spell
   print('Spell checking is', (vim.o.spell and 'enabled' or 'disabled'))
 end, { desc = '[T]oggle [S]pell' })
-
-
-
 
 -- A very yucky way of changing the theme depending of time.
 -- Checks the time and changes the theme on startup.
