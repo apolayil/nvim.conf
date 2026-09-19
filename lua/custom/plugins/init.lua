b307263f453e026639ca9b4f4a6e961aa368aec2
Arvin Dev <dev.arvin123@gmail.com>
2026-09-20 09:21:49 +1000

    Added custom plugins that were removed in conversion

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,13 @@
+vim.pack.add {
+	'https://github.com/junegunn/fzf',
+	'https://github.com/junegunn/fzf.vim',
+	'https://github.com/karb94/neoscroll.nvim',
+	'https://github.com/numToStr/Comment.nvim',
+}
+
+require('neoscroll').setup {}
+require('Comment').setup {}
+
 require 'custom.plugins.theme'
 require 'custom.plugins.auto-dark-mode'
 require 'custom.plugins.ghostty'
@@ -7,3 +17,5 @@ require 'custom.plugins.nvim-spider'
 require 'custom.plugins.precognition'
 require 'custom.plugins.smartyank'
 require 'custom.plugins.ufo'
+
+-- vim: ts=2 sts=2 sw=2 et
